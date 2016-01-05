2c6e327d66727bfdd03338162a6ec26f9aa5ba7f
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-01-05 03:31:41 -0500

    Fixed image load

--- assets/javascripts/views/car/CarCreateView.js
+++ assets/javascripts/views/car/CarCreateView.js
@@ -10,10 +10,13 @@ define(function(require) {
     template: template,
 
     ui: {
-      form: '#car-form'
+      form: '#car-form',
+      photo: '#photo',
+      output: '#output'
     },
 
     events: {
+      'change @ui.photo': 'openFile',
       'submit @ui.form': 'createCar'
     },
 
@@ -29,6 +32,7 @@ define(function(require) {
       var isModelUnique = this.isModelUnique.bind(this);
 
       if (isModelUnique(data)) {
+        if (this.photo) data.photo = this.photo;
         var carModel = new CarModel(data);
         this.carsCollection.add(carModel);
         Backbone.history.navigate('/', {trigger: true});
@@ -46,6 +50,22 @@ define(function(require) {
     isModelUnique: function(data) {
       var model = data.model;
       return !this.carsCollection.findWhere({model: model});
+    },
+
+
+    openFile: function(e) {
+      var input = event.target;
+      var reader = new FileReader();
+      var self = this;
+
+      reader.onload = function(){
+        var dataURL = reader.result;
+        var output = document.getElementById('output');
+        output.src = dataURL;
+        self.photo = dataURL;
+      };
+
+      reader.readAsDataURL(input.files[0]);
     }
   });
 
