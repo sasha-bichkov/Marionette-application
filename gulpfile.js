20337a351a802f257cc600e890a64c35476fc23e
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-01-09 07:10:27 -0500

    Removed comments from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-// загружаем необходимые модули
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-//создаём задачу 'browser-sync', чтобы поднять локальный сервер
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
@@ -11,8 +9,6 @@ gulp.task('browser-sync', function() {
   });
 });
 
-//когда вводим 'gulp' в консоли, то по умолчанию вызывается задача
-//'default'. Передаём ей задачу 'browser-sync' на вызов.
 gulp.task('default', ['browser-sync'], function() {
   console.log('Server has been started');
 });
